de759c6308c1114dff9d0d854e1e59b42e28438d
Darlan Thiago <darlanthiago53@gmail.com>
2021-11-29 13:32:13 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./style.css";
+
+import logo from './assets/img/logo.svg';
+
+function App() {
+
+  return (
+    <div className="App">
+      <img src={logo} alt="Travel Goiás" className="logoImage" />
+    </div>
+  )
+}
+
+export default App
